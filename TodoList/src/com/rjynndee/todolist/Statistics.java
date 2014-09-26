5f71d32e124820ebfbb02ee7295d11dfe4abc168
Ruby <rdejesus@ualberta.ca>
2014-09-26 16:14:57 -0600

    Update Statistics.java

--- TodoList/src/com/rjynndee/todolist/Statistics.java
+++ TodoList/src/com/rjynndee/todolist/Statistics.java
@@ -1,3 +1,6 @@
+/*this class holds the information of the controller saving the 
+details of the statistics like count and total todos */
+
 package com.rjynndee.todolist;
 
 public class Statistics{
